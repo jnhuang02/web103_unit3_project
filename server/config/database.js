68bfbd5d1e7480be59e3010ebc61fa4c0850509c
Justin Huang <justinstarwar@gmail.com>
2025-10-07 23:03:40 -0700

    update

--- server/config/database.js
+++ server/config/database.js
@@ -1,19 +1,4 @@
-// import pg from 'pg'
 
-// const config = {
-//     user: process.env.PGUSER,
-//     password: process.env.PGPASSWORD,
-//     host: process.env.PGHOST,
-//     port: process.env.PGPORT,
-//     database: process.env.PGDATABASE,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-// }
-
-// export const pool = new pg.Pool(config)
-
-// server/config/database.js
 import pg from 'pg'
 import dotenv from 'dotenv'
 
